4e5d27894c15b548c327c57e1dd8eb47832f8308
Danilo Silva <danilokovarike@gmail.com>
2025-06-25 21:15:10 -0300

    add initial files
    add initial files for created system

---
+++ Users.java
@@ -0,0 +1,9 @@
+abstract public class Users{
+    private String name;
+    private String email;
+    private boolean isAdmin = false;
+    private String password;
+    
+    public Users(String cname, cemail, cpassword){};
+    
+}
